a756146d393b4af8ac9d4b27d346687da18c69fe
zylideum <43623870+zylideum@users.noreply.github.com>
2022-10-25 22:01:18 -0700

    Finished 66. Plus One

--- 66. Plus One.py
+++ 66. Plus One.py
@@ -2,14 +2,36 @@ from typing import List
 
 
 class Solution:
+    # Abuses the ease of Python type assignments
+    # Leetcode shouldn't accept this since it returns
+    # an array of strings despite the type annotation
     def plusOne(self, digits: List[int]) -> List[int]:
-        if digits[-1] == 9:
-            digits[-2] == 1
-            digits[-1] == 0
-        else:
-            digits[-1] += 1
-        return digits
+        buildStr = ""
+        for num in digits:
+            buildStr += str(num)
+
+        buildStr = int(buildStr) + 1
+
+        return list(str(buildStr))
+
+    def neetCode(self, digits: List[int]) -> List[int]:
+        digits = digits[::-1]
+        one, i = 1, 0
+
+        while one:
+            if i < len(digits):
+                if digits[i] == 9:
+                    digits[i] = 0
+                else: 
+                    digits[i] += 1
+                    one = 0
+            else:
+                digits.append(1)
+                one = 0
+            i += 1
+        return digits[::-1]
 
 
 s = Solution()
-print(s.plusOne([9]))
+print(s.plusOne([1, 0, 9]))
+print(s.neetCode([1, 0, 9]))
